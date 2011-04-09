4eb98e7395c161128f286583a67ebd4d9e94fd64
Evgeny <evgeny@evgeny-laptop.(none)>
2011-04-09 11:47:06 +0200

    update webview

--- src/de/evgeny/classico/CompositionView.java
+++ src/de/evgeny/classico/CompositionView.java
@@ -2,9 +2,14 @@ package de.evgeny.classico;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.webkit.WebView;
 
 public class CompositionView extends Activity {
+	private static final String TAG = CompositionView.class.getSimpleName();
+	
 	WebView mWebView;
 	
 	@Override
@@ -16,5 +21,18 @@ public class CompositionView extends Activity {
 	    mWebView.getSettings().setBuiltInZoomControls(true);
 	    mWebView.loadUrl("http://members.home.nl/yourdesktop/highresolution/9.jpg");
 	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			
+			break;
 
+		default:
+			break;
+		}
+		Log.d(TAG, "don't touch me");
+		return super.onTouchEvent(event);
+	}
 }
