39c1037f24905cd20fe3fd527d9e8b9b1c9972ea
Evgeny <evgeny@evgeny-laptop.(none)>
2011-08-08 19:37:40 +0200

    remove old file

--- src/de/evgeny/classico/PartitureViewer.java
+++ src/de/evgeny/classico/PartitureViewer.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
-import java.util.Set;
 
 import org.apache.http.util.ByteArrayBuffer;
 
